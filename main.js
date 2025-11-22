6abebb0d959807319e382839a416f9829c0fed74
Marko Musica <marko.musica@emilfreydigital.hr>
2025-11-22 13:51:31 +0100

    Implemented calculator display logic

--- main.js
+++ main.js
@@ -1,11 +1,57 @@
-const digitsElement = document.querySelector(".digits");
-const operatorElement = document.querySelector(".operators");
+const digits = ["7", "8", "9", "4", "5", "6", "1", "2", "3", ".", "0", "="];
+const operators = ["+", "-", "*", "/"];
 
-const digits = [7, 8, 9, 4, 5, 6, 1, 2, 3, '•', 0, '='];
-const operators = ["+", "-", "*", "/"]; 
+createCalculator();
 
-createButtons(digitsElement, digits);
-createButtons(operatorElement, operators);
+const calculator = document.querySelector(".calculator");
+const display = document.querySelector(".display-text");
+let number = "";
+let operator = "";
+
+calculator.addEventListener("click", (e) => {
+  displayOnCalculator(e);
+  const element = e.target;
+  if (element.tagName === "BUTTON" && digits.includes(element.textContent)) {
+    if (number === "" && element.textContent === "0") {
+      return;
+    }
+    number += element.textContent;
+    console.log(number);
+  } else if (
+    element.tagName === "BUTTON" &&
+    operators.includes(element.textContent)
+  ) {
+    if (operator === "") {
+      operator = element.textContent;
+    }
+    console.log(operator);
+  }
+});
+
+function displayOnCalculator(e) {
+  const element = e.target;
+  const tagName = element.tagName;
+  const elementText = element.textContent.toLowerCase();
+  if (tagName === "BUTTON" && elementText === "clear") {
+    display.textContent = "";
+    number = "";
+    operator = "";
+  } else if (tagName === "BUTTON" && digits.includes(elementText)) {
+    display.textContent += elementText;
+  } else if (tagName === "BUTTON" && operators.includes(elementText)) {
+    if (operator === "") {
+      display.textContent += elementText;
+    }
+  }
+}
+
+function createCalculator() {
+  const digitsElement = document.querySelector(".digits");
+  const operatorElement = document.querySelector(".operators");
+  createButtons(digitsElement, digits);
+  createButtons(operatorElement, operators);
+}
+
 /**
  *
  * @param {Element} parentElement
